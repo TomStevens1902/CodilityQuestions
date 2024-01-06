aa8100cef66a486596fae0acfa7535893c1d9bd4
Tom <Tomstevens.19.02@gmail.com>
2024-01-06 15:47:53 +0000

    NailingPlanks
    .cpp added for nailing planks with:
    - Task score: 87%
    - Correctness: 100%
    - Performance: 75%

---
+++ Binary search algorithm/NailingPlanks.cpp
@@ -0,0 +1,54 @@
+#include <vector>
+using namespace std;
+
+struct PlankStruct {
+    int start;
+    int stop;
+};
+
+int solution(vector<int>& A, vector<int>& B, vector<int>& C) {
+    const int N = A.size();
+    const int M = C.size();
+    vector<PlankStruct> remaining(N);
+    vector<bool> nails(M, false);
+
+    // create plank struct
+    for (int i = 0; i < N; i++) {
+        remaining[i].start = A[i];
+        remaining[i].stop = B[i];
+    }
+
+    int minimum = -1;
+
+    // go through each nail, done once list is empty
+    vector<PlankStruct> planks;
+
+    for (int j = 0; j < M; j++) {
+        if (nails[C[j]]) {
+            continue;
+        }
+
+        nails[C[j]] = true;
+        planks = remaining;
+        remaining.clear();
+
+        for (int i = 0; i < (int)planks.size(); i++) {
+            if ((C[j] < planks[i].start) || (C[j] > planks[i].stop)) {
+                // Nail doesn't touch this plank 
+                remaining.push_back(planks[i]);
+            }
+            else {
+                // Nail is in plank, remove it from consideration
+                // by not adding it to remaining
+            }
+        }
+
+        // Nothing remains!  This was the final nail.
+        if (remaining.size() == 0) {
+            minimum = j + 1;
+            break;
+        }
+    }
+
+    return minimum;
+}
